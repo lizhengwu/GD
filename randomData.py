6bab666a6e5768c1ded8d1b22d49cc39d1bb3ad4
wangzengkai <wangzengkai@ppdai.com>
2020-07-28 21:26:33 +0800

    update

--- randomData.py
+++ randomData.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
 from common import PATH
+from random import choice, randint
 
-def randomPath():
-    print(PATH)
+# 获取随机目标数值
+def getRandomTargetData():
+    data = []
+    for _ in range(6):
+        d = {
+            'name': choice(PATH),
+            'target': randint(51, 100)
+        }
+        data.append(d)
+    
+    for item in data:
+        print(item)
+
 
 if __name__ == "__main__":
-    randomPath()
+    getRandomTargetData()
